3c5167011e6a3ffd0a669c9285a8b70a5b4d5d62
Seth Painter <Seth Painter>
2016-10-14 16:02:56 -0500

    Attempting to add html module and build everything

---
+++ html/src/com/thefoxarmy/rainbowwarrior/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.thefoxarmy.rainbowwarrior.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.thefoxarmy.rainbowwarrior.RainbowWarrior;
+
+public class HtmlLauncher extends GwtApplication {
+
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        return new GwtApplicationConfiguration(480, 320);
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new RainbowWarrior();
+    }
+}
